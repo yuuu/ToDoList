52b4f797cd2ebc364aae147f634b4604f4bcd99f
Yuhei Okazaki <yuuu@users.noreply.github.com>
2018-07-06 06:34:57 +0900

    create todo list

--- src/main.js
+++ src/main.js
@@ -1,13 +1,58 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from "vue";
-import App from "./App";
 
-Vue.config.productionTip = false;
+// https://jp.vuejs.org/v2/examples/todomvc.html
+var STORAGE_KEY = "todos-vuejs-demo";
+var todoStorage = {
+  fetch: function() {
+    var todos = JSON.parse(localStorage.getItem(STORAGE_KEY) || "[]");
+    todos.forEach(function(todo, index) {
+      todo.id = index;
+    });
+    todoStorage.uid = todos.length;
+    return todos;
+  },
+  save: function(todos) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
+  }
+};
 
-/* eslint-disable no-new */
-new Vue({
+const app = new Vue({
   el: "#app",
-  components: { App },
-  template: "<App/>"
+  data: {
+    // 使用するデータ
+    todos: []
+  },
+  methods: {
+    // 使用するメソッド
+    doAdd: function(event, value) {
+      var comment = this.$refs.comment;
+      if (!comment.value.length) {
+        return;
+      }
+      this.todos.push({
+        id: todoStorage.uid++,
+        comment: comment.value,
+        state: 0
+      });
+      comment.value = "";
+    },
+    doChangeState: function(item) {
+      item.state = item.state ? 0 : 1;
+    },
+    doRemove: function(item) {
+      var index = this.todos.indexOf(item);
+      this.todos.splice(index, 1);
+    }
+  },
+  watch: {
+    todos: {
+      handler: function(todos) {
+        todoStorage.save(todos);
+      },
+      deep: true
+    }
+  },
+  created() {
+    this.todos = todoStorage.fetch();
+  }
 });
